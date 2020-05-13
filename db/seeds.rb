b01e396eee5046f75d3d738f35ca11a4df16e812
coralie delpha <coralie.delpha@gmail.com>
2020-05-13 20:30:34 +0200

    seed 2 flats

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
+Flat.destroy_all
+
 Flat.create!(
   name: 'Light & Spacious Garden Flat London',
   address: '10 Clifton Gardens London W9 1DT',
   description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  picture_url: 'https://images.unsplash.com/photo-1464890100898-a385f744067f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
   price_per_night: 75,
   number_of_guests: 3
 )
@@ -10,6 +13,7 @@ Flat.create!(
   name: 'Cosy Parisian Studio',
   address: '16 villa Gaudelet 75011 Paris',
   description: 'A cosy studio with a beautiful view.',
+  picture_url: 'https://images.unsplash.com/photo-1529408632839-a54952c491e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
   price_per_night: 70,
   number_of_guests: 1
 )
