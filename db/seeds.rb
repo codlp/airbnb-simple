4df3d4a7e926ee6814ca8bff448e427ea53de8e7
coralie delpha <coralie.delpha@gmail.com>
2020-05-13 22:55:33 +0200

    styling index and show

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,35 @@
 Flat.destroy_all
 
-Flat.create!(
-  name: 'Light & Spacious Garden Flat London',
-  address: '10 Clifton Gardens London W9 1DT',
-  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
-  picture_url: 'https://images.unsplash.com/photo-1464890100898-a385f744067f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+puts "Starting to seed"
+
+flat = Flat.create!(
+  name: "Spacious condo",
+  address: "10 Liberty Street 75011 Paris",
+  description: "Spacious condo located in the heart of Paris's cooler neighborhood.",
+  picture_url: 'https://images.unsplash.com/photo-1494526585095-c41746248156?w=1200',
   price_per_night: 75,
   number_of_guests: 3
 )
 
-Flat.create!(
-  name: 'Cosy Parisian Studio',
-  address: '16 villa Gaudelet 75011 Paris',
-  description: 'A cosy studio with a beautiful view.',
-  picture_url: 'https://images.unsplash.com/photo-1529408632839-a54952c491e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
-  price_per_night: 70,
-  number_of_guests: 1
+flat = Flat.create!(
+  name: 'Stylish House',
+  address: '5 Queensmill Road London SW6 6JP',
+  picture_url: 'https://images.unsplash.com/photo-1493809842364-78817add7ffb?w=1200',
+  description: 'Lovely warm comfortable and stylishly furnished house. Private bedroom and bathroom with shared living areas.',
+  price_per_night: 65,
+  number_of_guests: 2
 )
+
+puts "Creating #{flat.name}"
+
+flat = Flat.create!(
+  name: 'Guest Suite',
+  address: 'Euston Rd London N1C 4QP',
+  picture_url: 'https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?w=1200',
+  description: 'A mini apartment within the clock tower apartment at St Pancras Station, with its own en suite bathroom and kitchen and sitting area.',
+  price_per_night: 110,
+  number_of_guests: 2
+)
+puts "Creating #{flat.name}"
+
+puts "Finished!"
